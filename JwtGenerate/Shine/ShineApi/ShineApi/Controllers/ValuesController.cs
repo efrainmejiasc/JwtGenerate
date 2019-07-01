00ebb887cbe71cde8464accbfcf4c72552b12efe
efrainmejiasc <efrainmejiasc@hotmail.com>
2019-07-01 19:11:06 -0400

    Update ValuesController.cs

--- JwtGenerate/Shine/ShineApi/ShineApi/Controllers/ValuesController.cs
+++ JwtGenerate/Shine/ShineApi/ShineApi/Controllers/ValuesController.cs
@@ -6,7 +6,6 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using ShineApi.Data;
-using ShineApi.Data;
 
 namespace ShineApi.Controllers
 {
