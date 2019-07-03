83ca39fc2b266d9618f241bd175fd927ff1a54b6
efrainmejiasc <efrainmejiasc@hotmail.com>
2019-07-03 15:17:03 -0400

    JWTupdate

---
+++ JwtGenerate/TestSoftware/TestJwtGenerate/TestJwtGenerate/CodeToVerification.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestJwtGenerate
+{
+    class CodeToVerification
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public DateTime SendDate { get; set; }
+        public DateTime VerificationDate { get; set; }
+        public bool Status { get; set; }
+        public string Code { get; set; }
+    }
+}
